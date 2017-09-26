41db6401fdfc6448131a6ec97c73917b354fec4b
anthony53 <32098530+anthony53@users.noreply.github.com>
2017-09-26 09:47:34 -0400

    Lab 4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        String command = "x";
+        System.out.println("HI IT'S ME MR.MESEEKS LOOK AT ME!!!!");
+        System.out.println("Existence is Pain tell me what i need to do so i can go away ");
+        command = input.next();
+        System.out.println("yes, right on it boss");
+    }
+}
